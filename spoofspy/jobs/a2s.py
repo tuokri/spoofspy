e3084df7eda30eca075e7ad171a376e0e82197aa
Tuomo <tuokri@tuta.io>
2023-08-02 17:54:02 +0300

    A2S wrapper stubs

--- spoofspy/jobs/a2s.py
+++ spoofspy/jobs/a2s.py
@@ -1,11 +1,14 @@
+from typing import Tuple
+
 import a2s
 
-def server_info():
-    a2s.info()
+
+def server_info(addr: Tuple[str, int]):
+    a2s.info(addr)
 
 
-def server_rules():
-    pass
+def server_rules(addr: Tuple[str, int]):
+    a2s.rules(addr)
 
 
 def server_players():
