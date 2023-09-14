d131c736462574d406a4541221810d59065c3229
Tuomo <tuokri@tuta.io>
2023-09-14 17:42:52 +0300

    Drop 1 worker

--- run-jobs.sh
+++ run-jobs.sh
@@ -4,6 +4,6 @@ set -x
 
 export OVERMIND_PROCFILE=Procfile-jobs
 export OVERMIND_ANY_CAN_DIE=1
-export OVERMIND_AUTO_RESTART=beat,worker1,worker2,worker3,a2s_worker1,a2s_worker2,a2s_worker3
+export OVERMIND_AUTO_RESTART=beat,worker1,worker2,a2s_worker1,a2s_worker2,a2s_worker3
 
 overmind start
