3bb4665cdeffd21a58238204aca0ff514721be02
Tuomo <tuokri@tuta.io>
2023-09-17 18:24:06 +0300

    Fix ping in prod env

--- deploy_jobs.ps1
+++ deploy_jobs.ps1
@@ -1,3 +1,4 @@
 hatch dep show requirements >requirements-jobs.txt
 fly deploy --region ams --dockerfile .\Dockerfile-jobs `
     --config fly.jobs.toml -a spoofspy-jobs --verbose
+fly ssh console --command "sysctl -w net.ipv4.ping_group_range='0 2147483647'" -a spoofspy-jobs
