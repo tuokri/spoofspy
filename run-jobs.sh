77ac3b69cc30bc26173c308da131b471bc4f1c40
Tuomo <tuokri@tuta.io>
2023-10-11 18:38:13 +0300

    Use .overmind.env file

--- run-jobs.sh
+++ run-jobs.sh
@@ -2,8 +2,4 @@
 
 set -x
 
-export OVERMIND_PROCFILE=Procfile-jobs
-export OVERMIND_ANY_CAN_DIE=1
-export OVERMIND_AUTO_RESTART=beat,worker1,worker2,a2s_worker1,a2s_worker2,a2s_worker3
-
 overmind start
