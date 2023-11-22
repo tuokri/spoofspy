c15ecd8d90b3afb8522e0daf44f32a322850446f
Tuomo <tuokri@tuta.io>
2023-11-22 23:19:51 +0200

    Trigger Sentry logging if Celery proc is force-restarted

--- monitor_celery_memory.py
+++ monitor_celery_memory.py
@@ -1,11 +1,17 @@
+import os
 import signal
 
 import psutil
+import sentry_sdk
 
 LIMIT = 300_000_000  # 300 MB.
 
+sentry_sdk.init(dsn=os.environ["SENTRY_DSN"])
+
 
 def main():
+    restarted_proc: int | None = None
+
     celery_procs = [
         process for process in psutil.process_iter()
         if "celery" in process.name()
@@ -17,8 +23,13 @@ def main():
             if rss > LIMIT:
                 print(f"WARNING: restarting '{proc}' '{proc.cmdline()}'")
                 proc.send_signal(signal.SIGHUP)
+                restarted_proc = proc.pid
         except Exception as e:
             print(f"error handling '{proc}': {e}")
+
+    if restarted_proc is not None:
+        raise RuntimeError(
+            f"had to force restart Celery process {restarted_proc}")
 
 
 if __name__ == "__main__":
