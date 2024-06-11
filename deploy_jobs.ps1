d3e49b6a033f6b1fb23e8b4e011626d7aa0cb649
Tuomo Kriikkula <tuokri@tuta.io>
2024-06-11 22:57:17 +0300

    Remove unsupported argument

--- deploy_jobs.ps1
+++ deploy_jobs.ps1
@@ -1,5 +1,5 @@
 hatch dep show requirements >requirements-jobs.txt
-fly deploy --region ams --dockerfile .\Dockerfile-jobs `
+fly deploy --dockerfile .\Dockerfile-jobs `
     --config fly.jobs.toml -a spoofspy-jobs --verbose
 fly ssh console --command "sysctl -w net.ipv4.ping_group_range='0 2147483647'" -a spoofspy-jobs
 fly ssh console --command "sysctl -p --system" -a spoofspy-jobs
