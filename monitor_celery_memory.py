32671a860957ec82e373d9f585b3fc2131e67934
Tuomo Kriikkula <tuokri@tuta.io>
2024-06-19 12:15:08 +0300

    Add comment regarding proc handling

--- monitor_celery_memory.py
+++ monitor_celery_memory.py
@@ -24,6 +24,9 @@ def main():
                 print(f"WARNING: restarting '{proc}' '{proc.cmdline()}'")
                 proc.send_signal(signal.SIGHUP)
                 restarted_proc = proc.pid
+                break
+                # If there are multiple leaky procs, handle the next one
+                # when this runs according to schedule.
         except Exception as e:
             print(f"error handling '{proc}': {e}")
 
